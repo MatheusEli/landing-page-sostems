8aecd8ddb5b8265e295fd423b4ba7546175bfefb
MatheusEli <matheuselisouzaferreira@hotmail.com>
2021-04-07 22:15:17 -0300

    implementando sobre-campanha

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from "@angular/core";
 import { HomeComponent } from "./home.component";
 import { HomeRoutingModule } from "./home-routing.module";
+import { SobreCampanhaComponent } from './sobre-campanha/sobre-campanha.component';
 
 @NgModule({
-    declarations: [HomeComponent],
+    declarations: [HomeComponent, SobreCampanhaComponent],
     imports: [CommonModule, FormsModule, HomeRoutingModule]
 })
 export class HomeModule{}
---
+++ src/app/home/sobre-campanha/sobre-campanha.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lp-sobre-campanha',
+  templateUrl: './sobre-campanha.component.html',
+  styleUrls: ['./sobre-campanha.component.scss']
+})
+export class SobreCampanhaComponent{}
