b1b06ec7f7761c165bdc76cd1aefad1b84bc3e9c
Matheus eli <matheuselisouzaferreira@hotmail.com>
2021-04-09 17:05:57 -0300

    configurando sobre a casa

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { HomeComponent } from "./home.component";
 import { HomeRoutingModule } from "./home-routing.module";
 import { SobreCampanhaComponent } from './sobre-campanha/sobre-campanha.component';
+import { SobreCasaComponent } from "./sobre-casa/sobre-casa.component";
 
 @NgModule({
-    declarations: [HomeComponent, SobreCampanhaComponent],
+    declarations: [HomeComponent, SobreCampanhaComponent, SobreCasaComponent],
     imports: [CommonModule, FormsModule, HomeRoutingModule]
 })
 export class HomeModule{}
---
+++ src/app/home/sobre-casa/sobre-casa.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'lp-sobre-casa',
+    templateUrl: './sobre-casa.component.html',
+    styleUrls: ['./sobre-casa.component.scss']
+})
+export class SobreCasaComponent{}
