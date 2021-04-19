cb93eecf316148e81bd61283abb8a8905f48481c
Matheus eli <matheuselisouzaferreira@hotmail.com>
2021-04-19 12:05:38 -0300

    implementando bloco doacoes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -8,9 +8,10 @@ import { SobreCasaComponent } from "./sobre-casa/sobre-casa.component";
 import { AtendimentosEspirituaisComponent } from "./atendimentos-espirituais/atendimentos-espirituais.component";
 import { AtendimentosSociaisComponent } from "./atendimentos-sociais/atendimentos-sociais.component";
 import { TransmissoesComponent } from "./transmissoes/transmissoes.component";
+import { ImportanciaDoacaoComponent } from "./importancia-doacao/importancia-doacao.component";
 
 @NgModule({
-    declarations: [HomeComponent, SobreCampanhaComponent, SobreCasaComponent, AtendimentosEspirituaisComponent, AtendimentosSociaisComponent, TransmissoesComponent],
+    declarations: [HomeComponent, SobreCampanhaComponent, SobreCasaComponent, AtendimentosEspirituaisComponent, AtendimentosSociaisComponent, TransmissoesComponent, ImportanciaDoacaoComponent],
     imports: [CommonModule, FormsModule, HomeRoutingModule]
 })
 export class HomeModule{}
---
+++ src/app/home/importancia-doacao/importancia-doacao.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'lp-doacao',
+    templateUrl: './importancia-doacao.component.html',
+    styleUrls: ['./importancia-doacao.component.scss']
+})
+export class ImportanciaDoacaoComponent{}
